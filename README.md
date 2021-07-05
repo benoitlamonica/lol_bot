288b1a01917c020bb14794ee1bc4d83e260aba2c
Benoit Lamonica <ben33127@gmail.com>
2021-07-05 12:35:48 +0200

    UPDT : Readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Discord LOL Bot
+# LOL Discord Bot
 
 ## This bot is used for characters info and more in the future.
+
+To add the bot to your Discord copy paste this link in your favorite browser : `https://discord.com/api/oauth2/authorize?client_id=861554332646637588&permissions=67584&scope=bot`
--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const { default: axios } = require('axios');
 const Express = require('express');
 const app = Express();
 const port = 1991;
+const champion = "Lulu"
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
+    axios.get(`https://ddragon.leagueoflegends.com/cdn/11.13.1/data/fr_FR/champion/${champion}.json`).then(rep => console.log(rep.data));
 })
 
 app.get('/', (req, res) => {
