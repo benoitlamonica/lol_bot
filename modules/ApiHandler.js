94ff907b99a985ef7bd9a5a5ded925fd3ed19bed
Benoit Lamonica <ben33127@gmail.com>
2021-07-06 20:45:50 +0200

    ADD : All champ + Some facto

--- modules/ApiHandler.js
+++ modules/ApiHandler.js
@@ -4,7 +4,8 @@ const Discord = require('discord.js');
 const VERSION = process.env.CLIENT_VERSION
 
 class ApiHandler {
-    static sendProperMessage = ({ msg, char }) => {
+
+    static sendProperMessage = (msg, char) => {
         axios.get(`https://ddragon.leagueoflegends.com/cdn/${VERSION}/data/fr_FR/champion/${char}.json`).then(rep => {
             let dataRep = rep.data;
             let data = dataRep.data[char];
@@ -15,9 +16,7 @@ class ApiHandler {
                 .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/${VERSION}/img/champion/${char}.png`)
                 .addField('\u200b', '\u200b')
                 .addField('Tips', ...data.allytips, true)
-                .addField('\u200b', '\u200b')
                 .addField('Jouer contre', ...data.enemytips, true)
-                .addField('\u200b', '\u200b')
                 .addField('Type', ...data.tags, true);
 
             let lastSkin = "";
@@ -33,6 +32,22 @@ class ApiHandler {
             console.log(err);
             msg.reply("Ce personnage n'existe pas !")
         });
+    }
+
+    static sendAllChar = (msg) => {
+        axios.get(`http://ddragon.leagueoflegends.com/cdn/${VERSION}/data/fr_FR/champion.json`).then(rep => {
+            let champObject = rep.data.data;
+            const reply = new Discord.MessageEmbed().setColor('#8558fb').setTitle(`Tous les champions à ce jour (${VERSION})`);
+            let content = ''
+            for (let champName in champObject) {
+                content += `${champName} \n\n`;
+            }
+            reply.setDescription(content);
+            msg.channel.send(reply);
+        }).catch(err => {
+            console.log(err);
+            msg.reply(`Erreur lors de la récupérations des personnages : ${err}`)
+        })
     }
 }
 
